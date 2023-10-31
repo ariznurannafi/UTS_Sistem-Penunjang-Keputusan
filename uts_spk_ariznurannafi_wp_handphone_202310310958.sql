daa16d3ee5fc76032b8fd7031e21f4acb08f6f06
ariznurannafi <138590884+ariznurannafi@users.noreply.github.com>
2023-10-31 12:01:03 +0700

    Add files via upload

---
+++ uts_spk_ariznurannafi_wp_handphone_202310310958.sql
@@ -0,0 +1,11 @@
+INSERT INTO "07TPLP015".uts_spk_ariznurannafi_wp_handphone (merek,ram,"Sistem Operasi",batrai,"Ukuran Layar",harga,"Memori Internal",alternatif,"Nilai Akhir","Rank") VALUES
+	 ('Apple iPhone 13','4 GB','iOS','3.240 mAh','6.1 inci',15000000,'512 GB',1.6334695,0.11627907,10),
+	 ('Samsung Galaxy S21 Ultra','12 GB','Android (One UI)','5.000 mAh','6.8 inci',18000000,'1 TB',3.1112974,0.13953489,2),
+	 ('OnePlus 9 Pro','8 GB','Android (OxygenOS)','4.500 mAh','6.7 inci',12000000,'256 GB',2.5368707,0.093023255,5),
+	 ('Xiaomi Mi 11','6 GB','MIUI (Android)','4.600 mAh','6.81 inci',9000000,'128 GB',2.7157426,0.069767445,4),
+	 ('Google Pixel 6','8 GB','Android (Pure Android)','4.614 mAh','6.4 inci',9000000,'128 GB',2.9362237,0.069767445,3),
+	 ('Oppo Find X3 Pro','12 GB','ColorOS (Android)','4.500 mAh','6.7 inci',14000000,'256 GB',2.0975573,0.10852713,9),
+	 ('Sony Xperia 1 III','16 GB','Android','4.500 mAh','6.5 inci',18000000,'256 GB',1.9630618,0.13953489,8),
+	 ('Asus ROG Phone 5','8 GB','Android (ROG UI)','6.000 mAh','6.78 inci',13000000,'512 GB',3.2714968,0.1007752,1),
+	 ('Huawei P50 Pro','8 GB','HarmonyOS (HongMeng OS)','4.360 mAh','6.6 inci',15000000,'256 GB',1.9833392,0.11627907,7),
+	 ('Realme GT Neo','8 GB','Android (Realme UI)','4.500 mAh','6.43 inci',6000000,'64 GB',2.0145242,0.046511628,6);
